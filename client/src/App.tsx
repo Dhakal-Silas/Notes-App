22ab86b1a85d78174b1bbf9a4e98be32ad1d787e
Silas <dhakalsilas1999@gmail.com>
2023-10-06 15:01:19 +0545

    create react app and  make ui

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <form className="note-form">
+        <input placeholder="Title" required />
+        <textarea placeholder="Content" rows={10} required />
+
+        <button type="submit">Add Note</button>
+      </form>
+      <div className="notes-grid">
+        <div className="note-item">
+          <div className="notes-header">
+            <button>x</button>
+          </div>
+          <h2>Note Title</h2>
+          <p>Note content</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
